d28f4704c7795e4bc84d8e2f2533e4ad4d7a9eaa
Alemu Beyene <149593675+alebey1@users.noreply.github.com>
2024-02-11 23:27:12 -0800

    Update index.php

--- admin/employee/index.php
+++ admin/employee/index.php
@@ -11,21 +11,17 @@ switch ($view) {
 	case 'list' :
 		$content    = 'list.php';		
 		break;
-
 	case 'add' :
 		$content    = 'add.php';		
 		break;
-
 	case 'edit' :
 		$content    = 'edit.php';		
 		break;
     case 'view' :
 		$content    = 'view.php';		
 		break;
-
 	default :
 		$content    = 'list.php';		
 }
 require_once ("../theme/templates.php");
 ?>
-  
